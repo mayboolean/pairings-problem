cdcac57d29a58a9210d3d9c7f3106d368b7a7cb5
mayboolean <>
2024-12-16 16:51:04 -0500

    finishes tech interview q

--- pairings.py
+++ pairings.py
@@ -1,6 +1,16 @@
 def find_pairs(num_string):
-  # Write your solution here!
-  pass
+  # split with " "
+  # convert to int
+  # sort
+  # append number to each number after it
+
+  nums = num_string.split(" ")
+  # print(nums)
+
+  nums_list = list(map(int, nums))
+  nums_list.sort()
+  
+  
 
 
 # Test cases
@@ -11,3 +21,18 @@ assert find_pairs("94") == set()
 
 print("All tests passed!")
 print("Finished early? Discuss time & space complexity")
+
+  # nums = num_string.split(" ")
+  # # print(nums)
+
+  # nums_int = []
+  # for num in nums:
+  #   nums_int.append(int(num))
+
+  # nums_int.sort()
+  # output = set()
+  # for i in range(len(nums_int)):
+  #   for j in range(i+1,len(nums_int)):
+  #     output.add((nums_int[i], nums_int[j]))   
+  # print(output)
+  # return output
